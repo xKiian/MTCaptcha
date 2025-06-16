86ca68183371a856c574b8b8e92bafa41c568e66
Kian <88455386+xKiian@users.noreply.github.com>
2025-06-16 23:43:53 +0200

    cleanup

--- main.go
+++ main.go
@@ -11,12 +11,13 @@ import (
 )
 
 func main() {
+	solver, _ := mtcaptcha.New("MTPublic-KzqLY1cKH", "2captcha.com", "")
 	
-	solver, _ := mtcaptcha.New("MTPublic-KzqLY1cKH", "2captcha.com", "http://207.244.217.165:6712")
 	token, err := solver.Solve()
 	if err != nil {
 		log.Fatal(err)
 	}
+	
 	fmt.Println("[+] Token:" + token[:100])
 	check(token)
 }
@@ -26,20 +27,26 @@ func check(token string) {
 		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
 	}
 	client := &http.Client{Transport: tr}
+	
 	body := strings.NewReader(fmt.Sprintf(`{"token":"%s"}`, token))
+	
 	req, err := http.NewRequest("POST", "https://2captcha.com/api/v1/captcha-demo/mtcaptcha/verify", body)
 	if err != nil {
 		log.Fatal(err)
 	}
+	
 	req.Header.Set("content-type", "application/json")
+	
 	resp, err := client.Do(req)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer resp.Body.Close()
+	
 	bodyText, err := io.ReadAll(resp.Body)
 	if err != nil {
 		log.Fatal(err)
 	}
+	
 	fmt.Printf("%s\n", bodyText)
 }
